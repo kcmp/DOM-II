8e61bd46788d8694a8da6f444d14fabc18897003
unknown <kcmp711@gmail.com>
2019-02-02 10:00:52 -0500

    added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+
+//navs mouseover (mouseenter/out)
+const link = document.querySelectorAll('a');
+console.log(link)
+for(i=0;i<link.length;i++){
+	link[i].addEventListener('mouseenter',function(event){
+		event.target.style.color = "blue";
+	})
+	link[i].addEventListener('mouseout', function(event){
+		event.target.style.color = "black";
+	})
+}
+// changes navbar to blue from black when mouse hovers over link
+
+
+//keydown
+const images = document.getElementsByTagName('img')
+console.log(images) 
+
+document.addEventListener('keydown', function(){
+    images[0].style.transform = "rotate(5deg)";
+})
+
+//when a key is pressed, top image rotates
+
